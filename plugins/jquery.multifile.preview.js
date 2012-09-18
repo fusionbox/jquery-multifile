4a4d397fe2d862d3e3a3074a014185d9a9f5dd67
Chris Proto <cproto@fusionbox.com>
2012-09-18 14:37:01 -0600

    consistent code style

--- plugins/jquery.multifile.preview.js
+++ plugins/jquery.multifile.preview.js
@@ -1,9 +1,10 @@
-;(function($, global, $multifile){
-
+;(function($, global, $multifile)
+{
   if ( !global.FileReader )
     return;
 
-  $multifile.templateCb = function(file){
+  $multifile.templateCb = function(file)
+  {
     var $tmpl = $('<p class="uploaded_image"> \
       <a href="" class="multifile_remove_input">x</a> \
       <span class="filename"></span> \
@@ -19,7 +20,8 @@
 
   $multifile.image_filter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
 
-  $multifile.fileReaderCb = function($tmpl, file_obj){
+  $multifile.fileReaderCb = function($tmpl, file_obj)
+  {
     return function(event)
     {
       if ( $multifile.image_filter.test(file_obj.type) )
